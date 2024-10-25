96b5f4b84412e6374eed02b91bdc6369669cc14f
DucTai <69153374+taicutm@users.noreply.github.com>
2024-10-25 21:01:06 +0700

    test

--- client/src/components/User/index.jsx
+++ client/src/components/User/index.jsx
@@ -521,6 +521,9 @@ export default function Student() {
           </div>
         )}
       </header>
+      <div>
+        <h1>Test123123123123</h1>
+      </div>
       {/* Hiển thị menu chính */}
       {showAllMenu && ( // hiển thị thông tin toàn bộ bên dưới menu
         <div className="container mx-auto py-8 px-4 md:px-32 flex flex-col md:flex-row">
