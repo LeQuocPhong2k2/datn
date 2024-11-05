d8c2ff04c196e3fbb06389e2bd607b37dd38f98c
DucTai <69153374+taicutm@users.noreply.github.com>
2024-11-05 19:53:07 +0700

    update lại xíu

--- client/src/api/Class.js
+++ client/src/api/Class.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL;
 
-const API_URL = process.env.REACT_APP_API_URL;
 
 async function addLopHoc(lopHoc) {
   console.log('lopHoc', lopHoc);
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,12 +1,3 @@
-const accountsRouter = require("./accounts");
-const teachersRouter = require("./teachers");
-const classesRouter = require("./class");
-const studentsRouter = require("./students");
-const subjectsRouter = require("./subjects");
-const leaveRequestRouter = require("./leaveRequest");
-const schedulesRouter = require("./schedules");
-
-<<<<<<< HEAD
 const accountsRouter = require('./accounts')
 const teachersRouter = require('./teachers')
 const classesRouter = require('./class')
@@ -24,10 +15,6 @@ function route(app) {
     app.use('/leaveRequest', leaveRequestRouter),
     app.use('/schedules', schedulesRouter)
   app.use('/attendance', attendanceRouter)
-=======
-function route(app) {
-  app.use("/accounts", accountsRouter), app.use("/teachers", teachersRouter), app.use("/class", classesRouter), app.use("/students", studentsRouter), app.use("/subjects", subjectsRouter), app.use("/leaveRequest", leaveRequestRouter), app.use("/schedules", schedulesRouter);
->>>>>>> 9317ac9e2b8d362d50437adde34bb75d1ff279fc
 }
 
-module.exports = route;
+module.exports = route
