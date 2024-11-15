f1be4f020eab01b030750e73f8ffab85d27457e2
LeQuocPhong2k2 <phonglehn2002@gmai.com>
2024-11-15 22:42:05 +0700

    UI Teacher

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Admin from './components/Admin';
 import Student from './components/User/index';
@@ -7,54 +9,61 @@ import ForgotPassword from './components/User/ForgotPassword';
 import Error from './components/Error';
 import Home from './components/Home';
 import ProtectedRoute from './middleware/ProtectedRoute';
+import Cookies from 'cookie-universal';
 
 import './App.css';
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const cookies = new Cookies();
+    const isLoggedIn = cookies.get('access_token');
+    if (!isLoggedIn) {
+      navigate('/login');
+    }
+  }, [navigate]);
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="" element={<Home />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/error" element={<Error />} />
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute allowedRole="Admin">
-                <Admin />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute allowedRole="Admin">
-                <Teacher />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/student"
-            element={
-              <ProtectedRoute allowedRole="Student">
-                <Student />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/teacher"
-            element={
-              <ProtectedRoute allowedRole="Teacher">
-                <Teacher />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/error" element={<Error />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedRole="Admin">
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedRole="Admin">
+              <Teacher />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/student"
+          element={
+            <ProtectedRoute allowedRole="Student">
+              <Student />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/teacher"
+          element={
+            <ProtectedRoute allowedRole="Teacher">
+              <Teacher />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
