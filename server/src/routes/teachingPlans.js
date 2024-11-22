b7c1e675803447036d609e809941c8ae8c1615b4
LeQuocPhong2k2 <phonglehn2002@gmai.com>
2024-11-23 01:02:27 +0700

    fix conflict after mer

--- server/src/routes/teachingPlans.js
+++ server/src/routes/teachingPlans.js
@@ -3,11 +3,7 @@ const router = express.Router();
 
 const TeachingPlanController = require("../app/controllers/TeachingPlanController");
 
-<<<<<<< HEAD
-router.post("/getTeachingPlanByTeacherAndByClassAndBySchoolYear", TeachingPlanController.getTeachingPlanByTeacherAndByClassAndBySchoolYear);
-=======
 router.post("/getTeachingPlanByTeacherAndByGradeAndBySchoolYear", TeachingPlanController.getTeachingPlanByTeacherAndByGradeAndBySchoolYear);
->>>>>>> 20843d1b23e93a1f5448cd60c5e4c29024bbf801
 
 router.post("/createTeachingPlan", TeachingPlanController.createTeachingPlan);
 
